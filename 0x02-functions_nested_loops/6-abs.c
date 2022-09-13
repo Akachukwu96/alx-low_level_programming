c550c4960b5cbd7d2a182e38e4b02a25cbcc3c65
Akachukwu <akachukwu.1220@gmail.com>
2022-09-13 13:37:18 -0700

    6. Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * _abs - a fuction that computes the absolute value of an integer
+ * @x: integer input
+ *
+ * Return: Always 0
+ */
+int _abs(int x)
+{
+	return (0);
+}
