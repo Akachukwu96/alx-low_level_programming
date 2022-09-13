ec7c9f520a66126f92357ef6089e4cf11ce552e9
Akachukwu <akachukwu.1220@gmail.com>
2022-09-13 10:17:55 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Prints the alphabet x10 with _putchar
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c = 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
