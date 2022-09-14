d0004fff1b21c99736f9b79f14c8c3c6d2e6719f
Akachukwu <akachukwu.1220@gmail.com>
2022-09-13 19:28:28 -0700

    0. putchar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * add - a function that adds two numbers and returns result
+ * @x: first number input
+ * @y: second number inpur
+ *
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
