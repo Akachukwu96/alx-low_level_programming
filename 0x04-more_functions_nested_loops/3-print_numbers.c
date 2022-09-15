cae838dd4e6a5e1c12621a2c4edb305816b5d7bc
Akachukwu <akachukwu.1220@gmail.com>
2022-09-15 12:05:49 -0700

    3. The numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - prints numbers 0 to 9 followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	while (c >= 48 && c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
