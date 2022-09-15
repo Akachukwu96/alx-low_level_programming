de399a20e99ae2c5d90e502381d07826eb0c768e
Akachukwu <akachukwu.1220@gmail.com>
2022-09-15 11:25:20 -0700

    2. Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * mul(int a, int b) - multiplies two integers
+ * @a: first interger input
+ * @b: second integer input
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	_putchar(a * b);
+}
