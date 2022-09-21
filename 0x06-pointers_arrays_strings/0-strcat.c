a6caa292bbce835c25acd263b71fa00489083b57
Akachukwu <akachukwu.1220@gmail.com>
2022-09-21 12:35:50 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
 	}
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
