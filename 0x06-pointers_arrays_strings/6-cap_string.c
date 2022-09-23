c8d31b2cd679ce3ca9643ce4c78fbd17ae7edea4
Akachukwu <akachukwu.1220@gmail.com>
2022-09-22 17:20:19 -0700

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,19 +12,18 @@ char *cap_string(char *s)
 	int a = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + a) >= 97 && *(s + a) <= 122)
-		*(s + a) -= 32;
-	/* *(s + a) is a pointer to s[a] */
+	if (s[a] >= 97 && s[a] <= 122)
+		s[a] -= 32;
 	a++;
 
-	while (*(s + a) != '\0')
+	while (s[a] != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + a) == sep_words[i])
+			if (s[a] == sep_words[i])
 			{
-				if ((*(s + a) >= 97 && *(s + a) <= 122))
-				*(s + a) -= 32;
+				if (s[a] >= 97 && s[a] <= 122)
+					s[a] -= 32;
 				break;
 			}
 		}
