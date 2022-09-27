7563c7e2dd4ecfc4e73247cb632fd791a2a45631
Akachukwu <akachukwu.1220@gmail.com>
2022-09-26 19:55:42 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,4 +11,19 @@
  * only of bytes from accept.
  */
 unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, len;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+			}
+			return (len);
+		}
+	}
+}
 
