9ef04324c36c66207251c86c3a4913cfe09bf4a3
Akachukwu <akachukwu.1220@gmail.com>
2022-09-26 20:05:55 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 
-
 /**
  * _strspn - gets the length of a prefix substring
  * @s: main string to be scanned
  * @accept: string containing the list of characters to be matched
- *
  * Return: number of bytes in the initial segment of s which consist
  * only of bytes from accept.
  */
