cccd22ad96f327d4c25dfb4fb7a7549f659aab39
Akachukwu <akachukwu.1220@gmail.com>
2022-09-30 09:10:33 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: count of arguments applied to a program
+ * @argv: array of strings
+ * Return: Always (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
+	}
+}
