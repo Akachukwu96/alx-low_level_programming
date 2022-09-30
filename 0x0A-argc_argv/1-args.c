8889304402681b2b0572b68e823610cb89fd2a22
Akachukwu <akachukwu.1220@gmail.com>
2022-09-30 06:51:19 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments supplied to the program
+ * @argv: an array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
