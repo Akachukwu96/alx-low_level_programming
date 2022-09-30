13f23f118c05bb99f1986c75161b6d93bc733226
Akachukwu <akachukwu.1220@gmail.com>
2022-09-30 06:36:23 -0700

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * @argv: an array of string
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0];
+	printf("%s\n", argv[0]);
 	return (0);
 }
