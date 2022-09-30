2c3d687cf237580f8c5caf18c6e5bee4af857956
Akachukwu <akachukwu.1220@gmail.com>
2022-09-30 07:18:35 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - prints all arguments it receives
+ * @argc: count of argument applied to a program
+ * @argv: array of strings
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
