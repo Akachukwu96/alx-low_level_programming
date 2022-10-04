dc8462dbdbbd540b9570208bcffdf9e1417c1024
Akachukwu <akachukwu.1220@gmail.com>
2022-10-03 20:19:58 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,22 +11,24 @@
  */
 char *_strdup(char *str)
 {
-	char *s;
 	char *ds; /* pointer to duplicate string */
-	int len; /* len is length of string */
+	int len, j; /* len is length of string */
 
-	s = malloc(len * sizeof(str)); /* memory for string */
-
-	if (s == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	ds = s; /* assigns or copies s to ds */
-	while (*str)
+	while (len = 0 && str[len] != '\0')
 	{
-		*ds = *str;
-		ds++;
-		str++;
+		len++;
 	}
-	*ds = '/0'; /* assigns null terminator to ds */
-	return (s);
+	ds = (char *)malloc(len + 1 * sizeof(char)); /* memory for string */
+
+	if (ds == NULL)
+		return (NULL);
+
+	for (j = 0; j <= len; j++)
+	{
+		ds[j] = str[len];
+	}
+	return (ds);
 }
