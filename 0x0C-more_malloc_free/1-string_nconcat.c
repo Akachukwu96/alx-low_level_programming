455d8bfa2bca8ea5614cd297d74811fb9f508c89
Akachukwu <akachukwu.1220@gmail.com>
2022-10-06 18:36:41 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,37 +19,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
+
 	while (s2[j] != '\0')
-	{
 		j++;
-	}
-	l = i + n;
+
+	if (n >= j)
+		l = i + j;
+	else
+		l = i + n;
 	s3 = malloc(sizeof(char) * l + 1);
-
 	if (s3 == NULL)
-	{
 		return (NULL);
-	}
-
-	for (k = 0; k < l; k++)
-		if (k < i)
-			s3[k] = s1[k];
-
-	for (n = 0; n < j; n++)
+	j = 0;
+	while (k < l)
 	{
-		if (n >= j)
+		if (k <= i)
+			s3[k] = s1[k];
+		if (k >= i)
 		{
 			s3[k] = s2[j];
-			k++;
 			j++;
 		}
-		s3[k] = s2[n];
 		k++;
 	}
+	s3[k] = '\0';
 	return (s3);
 }
