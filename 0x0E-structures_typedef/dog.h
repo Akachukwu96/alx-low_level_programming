2b1eca31d3c41fd9630c12d8fa45187c59b587e8
Akachukwu <akachukwu.1220@gmail.com>
2022-10-10 18:05:54 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,11 +3,11 @@
 
 /**
  * struct my_dog - gives information about a dog
- * @*name: name of the dog
+ * @name: name of the dog
  * @age: age of the dog
- * @*owner: name of the dog's owner
+ * @owner: name of the dog's owner
  *
- * Description: struct called "dog" that stores its name, age 
+ * Description: struct called "dog" that stores its name, age
  * and the name of the owner
  */
 struct my_dog
