d3d79380a544ec9b28385afe96c3c429ca72cb0b
Akachukwu <akachukwu.1220@gmail.com>
2022-10-12 20:47:29 -0700

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function to be called back
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(char *);
+}
