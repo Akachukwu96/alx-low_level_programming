e3aafa0318d079308368331f942bdd8c4e9cc26e
Akachukwu <akachukwu.1220@gmail.com>
2022-10-12 21:56:13 -0700

    1-array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int s;
 	if (array && action)
 		for (s = 0; s < size; s++)
-	if (array[s] && action)
+
 	action(array[s]);
 }
