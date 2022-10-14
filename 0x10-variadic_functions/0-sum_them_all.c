74b38ffb37ce152d596581a536427ff7e92a83ed
Akachukwu <akachukwu.1220@gmail.com>
2022-10-13 17:50:36 -0700

    0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,23 +6,26 @@
  * sum_them_all - sum of all its parameters
  * @n: count of all the parameters
  *
- * Return: sum
+ * Return: result
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	if (n == 0)
 		return (0);
 
-	va_list numbers; /* named the list numbers */
-	int i, sum;
+	if (n > 0)
+	{
+		va_list numbers; /* named the list numbers */
+		int i, result;
 
-	va_start(numbers, n); /* Initialize the argument list */
-	sum = 0;
+		va_start(numbers, n); /* Initialize the argument list */
+		result = 0;
 
-	for (i = 0; i < n; i++)
-		sum = sum + va_arg(numbers, int); /* the next arg after n */
+		for (i = 0; i < n; i++)
+			result += va_arg(numbers, int); /* the next arg */
 
-	va_end(numbers);
-	return (sum);
+		va_end(numbers);
+	}
+	return (result);
 }
 
