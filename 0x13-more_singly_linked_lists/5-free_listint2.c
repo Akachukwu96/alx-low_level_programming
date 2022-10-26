fbc6ad93b831c7ce2c4cf33bddf126aedf9fb52e
Akachukwu <akachukwu.1220@gmail.com>
2022-10-25 20:09:58 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,13 +2,14 @@
 
 /**
   *free_listint2 - function that frees a listint_t list.
-  * @head - pointer to the list to be freed
+  * @head: pointer to the list to be freed
   *
   * Return: void
   */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (head == NULL)
 		return;
 
