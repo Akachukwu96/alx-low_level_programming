191843c66db432cea99293a70eb0a60cde9ecb2c
Akachukwu <akachukwu.1220@gmail.com>
2022-10-30 15:31:37 -0700

    2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,10 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
 
-		if (d->owner)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)\n", d->owner);
+			printf("Owner: (nil)\n");
 	}
 }
 
