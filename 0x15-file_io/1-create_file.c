28ac640a3e89081acbc2d59b09ae6bfb90fdf3dc
Akachukwu <akachukwu.1220@gmail.com>
2022-10-31 18:46:35 -0700

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+  * create_file - creates file
+  * @filename: name of the file
+  * @text_content: string to write to the file
+  * Return: 1 on Sucess or -1 on failure.
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int clen, rw_only;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (clen = 0; filename[clen]; clen++)
+		;
+
+	rw_only = write(fd, text_content, clen);
+
+	if (rw_only == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
